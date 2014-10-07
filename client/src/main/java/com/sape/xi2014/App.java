a3c8ebf1987bf3319ceb1fb1c23dd245512b795f
unknown <spras3@BLRSPRAS3118258.sapient.com>
2014-10-07 15:03:50 +0530

    Web Client Service App

--- client/src/main/java/com/sape/xi2014/App.java
+++ client/src/main/java/com/sape/xi2014/App.java
@@ -1,5 +1,13 @@
 package com.sape.xi2014;
 
+import static spark.Spark.get;
+import static spark.SparkBase.setPort;
+
+import com.google.gson.Gson;
+import com.sape.xi2014.entity.ClientResponse;
+import com.sape.xi2014.flow.b.withmicrosvc.ImperativeServiceMediator;
+
+
 /**
  * Hello world!
  *
@@ -8,6 +16,29 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	setPort(4569);
+
+		get("/hello-client", (req, res) -> "hello world from the web client");
+
+		get("imperative/search", (request, response) -> {
+
+			Object returnValue = null;
+			String searchTerm = null;
+			ImperativeServiceMediator serviceAgg = new ImperativeServiceMediator();
+			ClientResponse aggregatedResponse = null;
+			try {
+				searchTerm = request.queryParams("searchTerm");
+				aggregatedResponse = serviceAgg.getAggregatedResponse(searchTerm);
+				Gson j = new Gson();
+				returnValue = j.toJson(aggregatedResponse.getTiles().getTiles());
+				
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new RuntimeException("encountered an exception while trying to fetch reviews for seller ", e);
+			}
+
+			return returnValue;
+		});
     }
 }
