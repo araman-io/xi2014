ee2a86038801c736a19e3f44bb1bbcb586675d84
sagarprasad <sagar.prasad@gmail.com>
2014-10-09 15:38:26 +0530

    Adding new version of hysterix pom entry

--- client/src/main/java/com/sape/xi2014/flow/e/frpwithmicrosvcasyncresilient/stub/ResilentProductImageServiceClient.java
+++ client/src/main/java/com/sape/xi2014/flow/e/frpwithmicrosvcasyncresilient/stub/ResilentProductImageServiceClient.java
@@ -3,8 +3,6 @@ package com.sape.xi2014.flow.e.frpwithmicrosvcasyncresilient.stub;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import rx.Observable;
-
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
 import com.netflix.hystrix.HystrixCommandKey;
